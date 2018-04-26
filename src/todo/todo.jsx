cc7a7062c7709439184455a300774c1d196590d2
Elton Mozart <elton.mozart@gmail.com>
2018-04-26 18:12:40 -0300

    Implementação de novas funções
    Implementadas funções para remover, marcar como feita e marcar como pendente. Também foi implementado uma função de refresh da lista de tarefas.

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -13,13 +13,39 @@ export default class Todo extends Component {
         super(props)
         this.state = { description: '', list: [] }
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
     }
+
+    refresh() {
+        Axios.get(`${URL}?sort=-createdAt`)
+            .then( resp => this.setState({...this.state, description: '', list: resp.data}) )
+    }    
+
 
     handleAdd(){
         const description = this.state.description
         Axios.post(URL, { description })
-            .then(res => console.log('funcionou!!'))
+            .then(res => this.refresh())
+    }
+
+    handleRemove(todo) {
+        Axios.delete(`${URL}/${todo._id}`)
+            .then( resp => this.refresh() )
+    }
+
+    handleMarkAsDone(todo) {
+        Axios.put(`${URL}/${todo._id}`, {...todo, done: true})
+            .then( resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo) {
+        Axios.put(`${URL}/${todo._id}`, {...todo, done: false})
+            .then( resp => this.refresh())
     }
 
     handleChange(e){
@@ -30,9 +56,17 @@ export default class Todo extends Component {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm description={this.state.description}  handleAdd={this.handleAdd}
+
+                <TodoForm 
+                    description={this.state.description}    
+                    handleAdd={this.handleAdd}
                     handleChange={this.handleChange}/>
-                <TodoList/>
+
+                <TodoList 
+                    list={this.state.list} 
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
+                    handleRemove={this.handleRemove} />
             </div>
         )
     }
